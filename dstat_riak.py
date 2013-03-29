c8a37d93b8c413ba4a9a6915b021b8212c239d88
James Martin <jmartin@basho.com>
2013-03-29 11:31:47 -0400

    pep8 compliant.

--- dstat_riak.py
+++ dstat_riak.py
@@ -1,5 +1,6 @@
 ### Author: James Martin <jmartin@basho.com>
 
+
 class dstat_plugin(dstat):
     """
     Riak Stats counter
@@ -8,12 +9,12 @@ class dstat_plugin(dstat):
     """
     def __init__(self):
         self.name = 'riak'
-        self.nick = ('gets','glat', 'puts', 'plat')
-        self.vars = ('node_gets_total', 'node_get_fsm_time_mean',  'node_puts_total','node_put_fsm_time_mean')
+        self.nick = ('gets', 'glat', 'puts', 'plat')
+        self.vars = ('node_gets_total', 'node_get_fsm_time_mean',
+                     'node_puts_total', 'node_put_fsm_time_mean')
         self.type = 'd'
         self.width = 6
         self.scale = 50
-
 
     def check(self):
         try:
@@ -21,21 +22,23 @@ class dstat_plugin(dstat):
             import urllib2
             global json, urllib2
         except:
-            raise Exception, 'Plugin needs json and urllib2 module'
+            raise Exception('Plugin needs json and urllib2 module')
 
     def extract(self):
         try:
-            stats_raw = urllib2.urlopen('http://127.0.0.1:8098/stats', None, 5).read()
+            stats_raw = urllib2.urlopen(
+                'http://127.0.0.1:8098/stats', None, 5).read()
         except:
-            raise Exception, 'Make sure you can access your riak stats interface'
+            raise Exception(
+                'Make sure you can access your riak stats interface')
 
-        stats=json.loads(stats_raw)
-
+        stats = json.loads(stats_raw)
 
         for name in self.vars:
             if name.find('_total') != -1:
                 self.set2[name] = float(stats[name])
-                self.val[name] = (self.set2[name] - self.set1[name]) * 1.0 / elapsed
+                self.val[name] = (
+                    self.set2[name] - self.set1[name]) * 1.0 / elapsed
             else:
                 self.val[name] = long(stats[name])
 
